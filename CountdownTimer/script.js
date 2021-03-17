8b5c5205fd90030e8a860c4b723ea6f8a94dff77
Benjamin Aboagye <55924371+aben-20@users.noreply.github.com>
2021-03-17 23:16:46 +0000

    Update script.js

--- CountdownTimer/script.js
+++ CountdownTimer/script.js
@@ -10,25 +10,23 @@ function countdown() {
     const currentDate = new Date();
 
     const totalSeconds = (newYearsDate - currentDate) / 1000;
-
     const days = Math.floor(totalSeconds / 3600 / 24);
     const hours = Math.floor(totalSeconds / 3600) % 24;
     const mins = Math.floor(totalSeconds / 60) % 60;
     const seconds = Math.floor(totalSeconds) % 60;
 
-
     daysEl.innerHTML = days
     hoursEl.innerHTML = hours
     minsEl.innerHTML = mins
     secondsEl.innerHTML = seconds
-
 }
 
+function currDate() {
+    var d = new Date().toDateString()
+    document.getElementById("_date").innerHTML = d;
+}
+
+currDate();
 countdown();
 
 setInterval(countdown, 1000);
-
-
-
-
-
