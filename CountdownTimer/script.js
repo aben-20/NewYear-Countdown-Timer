58264a8e8bdefbadd16de4610f2f7252df065eb1
Benjamin Aboagye <55924371+aben-20@users.noreply.github.com>
2021-03-20 15:53:18 +0000

    Update script.js

--- CountdownTimer/script.js
+++ CountdownTimer/script.js
@@ -1,7 +1,7 @@
-const daysEl      = document.getElementById('days');
-const hoursEl     = document.getElementById('hours');
-const minsEl      = document.getElementById('mins');
-const secondsEl   = document.getElementById('seconds');
+const _days      = document.getElementById('days');
+const _hours     = document.getElementById('hours');
+const _mins      = document.getElementById('mins');
+const _seconds   = document.getElementById('seconds');
 
 const newYears = "1 Jan 2022";
 
@@ -15,18 +15,70 @@ function countdown() {
     const mins = Math.floor(totalSeconds / 60) % 60;
     const seconds = Math.floor(totalSeconds) % 60;
 
-    daysEl.innerHTML = days
-    hoursEl.innerHTML = hours
-    minsEl.innerHTML = mins
-    secondsEl.innerHTML = seconds
+    _days.innerHTML = days
+    _hours.innerHTML = double_zeros(hours)
+    _mins.innerHTML = double_zeros(mins) 
+    _seconds.innerHTML = double_zeros(seconds)
 }
 
-function currDate() {
-    var d = new Date().toDateString()
-    document.getElementById("_date").innerHTML = d;
+function double_zeros(time) {
+    return time < 10 ? (`0${time}`): time;
 }
 
-currDate();
+const date = new Date();
+const renderCalendar = () => {
+  date.setDate(1);
+  const monthDays = document.querySelector(".days");
+
+  const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+
+  const prevLastDay = new Date(date.getFullYear(),date.getMonth(),0).getDate();
+
+  const firstDayIndex = date.getDay();
+
+  const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1,0).getDay();
+
+  const nextDays = 7 - lastDayIndex - 1;
+
+  const months = ["January","February","March","April","May","June","July","August",
+  "September","October","November","December"];
+
+  document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+
+  document.querySelector(".date p").innerHTML = new Date().toDateString();
+
+  let days = "";
+
+  for (let x = firstDayIndex; x > 0; x--) {
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+  }
+
+  for (let i = 1; i <= lastDay; i++) {
+    if (i === new Date().getDate() &&date.getMonth() === new Date().getMonth())
+     {
+      days += `<div class="today">${i}</div>`;
+    } else {
+      days += `<div>${i}</div>`;
+    }
+  }
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<div class="next-date">${j}</div>`;
+    monthDays.innerHTML = days;
+  }
+};
+
+document.querySelector(".prev").addEventListener("click", () => {
+  date.setMonth(date.getMonth() - 1);
+  renderCalendar();
+});
+
+document.querySelector(".next").addEventListener("click", () => {
+  date.setMonth(date.getMonth() + 1);
+  renderCalendar();
+});
+
+
+renderCalendar();
+double_zeros();
 countdown();
-
 setInterval(countdown, 1000);
